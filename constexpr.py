def2d64cde3d1319609df859ef5ab5308da572ca
Bodale Denis <bodaledenis.work@gmail.com>
2021-08-15 00:38:32 +0300

    Initial coomit with the project

---
+++ constexpr.py
@@ -0,0 +1,19 @@
+# Constants used in the application 
+
+VOWELS = ['a', 'A', 'E', 'e', 'i', 'I', 'o', 'O', 'u', 'U']
+
+CONSONATES = ['b', 'B', 'c', 'C', 'd', 'D', 'f', 'F', 'g', 'G',
+              'h', 'H', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M',
+              'n', 'N', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S',
+              't', 'T', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y',
+              'z', 'Z']
+
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+SPECIAL_CHAR = ['~', '!', '#', '^', '*', '(', ')', '-', '+', '.',
+                '?', ':', ';', '<', '>', '[', ']', '_', '/', ',']
+
+PASSWORD_LENGTH = 10
+
+KEY_FILE = 'KeyFile.key'
+MY_DATA_FILE = 'My Data'
